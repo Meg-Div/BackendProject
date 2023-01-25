f3b0eb8679c560f9d105c6edcd32b8de8f927e22
Megan <meganthielman@gmail.com>
2023-01-25 07:37:03 -0800

    index.js updates

---
+++ seeders/20230125002558-Users.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("Users", [
+      {
+        username: "MegFromWalnutCreek",
+        firstname: "Meg",
+        lastname: "Divringi",
+        password: "ABC123",
+        admin: True,
+        zip: 94596,
+        districtid: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        username: "Frito",
+        firstname: "James",
+        lastname: "Jamerson",
+        password: "password",
+        admin: False,
+        zip: 94598,
+        districtid: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
