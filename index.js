75fd1a81c67d3b0a90fa81a54f8100bd616353f2
Megan <meganthielman@gmail.com>
2023-01-25 11:37:08 -0800

    Routes Update

--- index.js
+++ index.js
@@ -3,150 +3,18 @@ const app = express();
 const PORT = 3009;
 
 const session = require("express-session");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const { Users } = require("./sequelize/models");
-const models = require("./sequelize/models");
-
-const bcrypt = require("bcrypt");
-const router = express.Router();
-
-//middleware
-app.use(express.json());
-
-// configure the app to use bodyParser()
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-app.use(bodyParser.json());
-
-app.use(cookieParser());
-
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
-const store = new SequelizeStore({ db: models.sequelize });
-
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-    store: store,
-    /*
-    cookie: {
-      secure: false,
-      //milliseconds
-      maxAge: 1592000000,
-    },
-    */
-  })
-);
-
-store.sync();
+const routes = require("./routes/routes");
 app.set("view engine", "ejs");
 
-//custom middleware
+//route to the files in Routes folder
+app.use("/admin", routes);
+app.use("/create", routes);
+app.use("/home", routes);
+app.use("/hub", routes);
+app.use("login", routes);
+app.use("/review", routes);
+app.use("/youvoted", routes);
 
-const authenticate = (req, res, next) => {
-  if (req.session.user) {
-    next();
-  } else {
-    res.redirect("/login");
-  }
-};
+//localhost:3006/clinic/get_clinic
 
-//routes
-
-//about us
-
-app.get("/home", authenticate, (req, res) => {
-  res.render("pages/home", {});
-});
-
-//create
-app.get("/create", authenticate, (req, res) => {
-  const { firstname, lastname, username, password, zip } = req.body;
-  bcrypt.hash(password, 10, async (err, hash) => {
-    const dist = 1;
-    zip[0] < 5 ? dist == 1 : dist == 2;
-    const user = await Users.create({
-      firstname: firstname,
-      lastname: lastname,
-      username: username,
-      password: hash,
-      zip: zip,
-      district: dist,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    res.status(200).render("pages/login");
-  });
-  res.render("pages/create", {});
-});
-
-router.post("/create_user", (req, res) => {
-  const { firstName, lastName, userName, password } = req.body;
-  bcrypt.hash(password, 10, async (err, hash) => {
-    const user = await Users.create({
-      firstName: firstName,
-      lastName: lastName,
-      userName: userName,
-      password: hash,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    res.status(200).render("pages/login");
-  });
-});
-
-app.get("/admin", authenticate, (req, res) => {
-  res.render("pages/admin", {});
-});
-
-app.get("/hub", authenticate, (req, res) => {
-  res.render("pages/hub", {});
-});
-
-app.get("/review", authenticate, (req, res) => {
-  res.render("pages/review", {});
-});
-
-app.get("/youvoted", authenticate, (req, res) => {
-  res.render("pages/youvoted", {});
-});
-
-//logout
-app.post("/logout", (req, res) => {
-  if (req.session) {
-    req.session.destroy((err) => {
-      res.redirect("/login");
-    });
-  }
-});
-
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  console.log(req.body);
-  const user = await Customers.findOne({
-    where: {
-      username: username,
-    },
-  });
-  bcrypt.compare(password, user.password, (err, result) => {
-    if (err) {
-      res.send(err);
-      return;
-    }
-    if (!result) {
-      res.status(401).send("Your password does not match.");
-      res.redirect("/login");
-      return;
-    }
-  });
-});
-
-module.exports = router;
-
-app.listen(3006, console.log(`logged into port 3006`));
+app.listen(PORT, console.log(`listening on port ${PORT}`));
---
+++ routes/routes.js
@@ -0,0 +1,154 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+
+const { Users, Positions, Districts } = require("../models");
+const models = require("../models");
+
+//use postman:
+//localhost:3007/auth/get_clinic
+
+const session = require("express-session");
+const bodyParser = require("body-parser");
+
+//middleware
+router.use(express.json());
+
+// configure the app to use bodyParser()
+router.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+router.use(bodyParser.json());
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const store = new SequelizeStore({ db: models.sequelize });
+
+router.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+  })
+);
+store.sync();
+
+//custom middleware
+
+const authenticate = (req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
+
+//admin:
+router.get("/admin", authenticate, (req, res) => {
+  res.render("pages/admin", {});
+});
+
+//home:
+router.get("/home", authenticate, (req, res) => {
+  res.render("pages/home", {});
+});
+
+//login:
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  console.log(req.body);
+  const user = await Users.findOne({
+    where: {
+      username: username,
+    },
+  });
+  const match = bcrypt.compare(password, user.password, (err, result) => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    if (!result) {
+      res.status(401).send("Your password is incorrect.");
+      res.redirect("/login");
+      return;
+    }
+  });
+  if (match) {
+    req.session.user = user;
+    res.redirect("/hub");
+  }
+});
+
+//logout:
+router.post("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      res.redirect("/login");
+    });
+  }
+});
+
+//hub
+router.get("/hub", authenticate, async (req, res) => {
+  console.log(req.session.user);
+  const user = await Users.findOne({
+    where: {
+      username: req.session.user.username,
+    },
+  });
+  const district = await Districts.findOne({
+    where: {
+      districtid: req.session.user.districtid,
+    },
+  });
+  const position = await Positions.findOne({
+    where: {
+      districtid: req.session.user.districtid,
+    },
+  });
+
+  console.log(p);
+  res.render("pages/hub", {
+    user: user,
+    district: district,
+    position: position,
+  });
+});
+
+//review
+router.post("/review", authenticate, (req, res) => {
+  if (form.body.length < 1) {
+    res.status(400);
+  }
+  res.render("pages/review", {});
+});
+
+//you voted
+router.get("/youvoted", authenticate, (req, res) => {
+  res.render("pages/youvoted", {});
+});
+
+//create
+router.get("/create", authenticate, (req, res) => {
+  const { firstname, lastname, username, password, zip } = req.body;
+  bcrypt.hash(password, 10, async (err, hash) => {
+    const dist = 1;
+    zip[0] < 5 ? dist == 1 : dist == 2;
+    const user = await Users.create({
+      firstname: firstname,
+      lastname: lastname,
+      username: username,
+      password: hash,
+      zip: zip,
+      districtid: dist,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    res.status(200).render("pages/login");
+  });
+  res.render("pages/create", {});
+});
+
+module.exports = router;
